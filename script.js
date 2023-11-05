96fd4a258f9451b95e727dc915563e6a52c03635
reprise77 <semenzemskov03@yandex.ru>
2023-11-05 20:20:18 +1000

    add page "About the site", add modal windows in "Profile"

--- script.js
+++ script.js
@@ -5,27 +5,64 @@ const loginOption = document.getElementById('loginOption');
 const accountOption = document.getElementById('accountOption');
 const loginForm = document.getElementById('loginForm');
 const registrationForm = document.getElementById('registrationForm');
+const changeNickButton = document.getElementById('changeNickButton');
+const overlay = document.getElementById('overlay');
+const changeUsername = document.getElementById('change-username');
+const closeChange = document.getElementById('closechange');
+
+let loginFormVisible = false;
 
 loginButton.addEventListener('click', () => {
     loginPopup.style.display = 'block';
+    if (!loginFormVisible) {
+        loginOption.style.display = 'block'
+        accountOption.style.display = 'block'
+    }
 });
 
 closePopup.addEventListener('click', () => {
     loginPopup.style.display = 'none';
+    registrationForm.style.display = 'none';
+    loginForm.style.display = 'none';
+    loginFormVisible = false;
 });
 
 loginOption.addEventListener('click', () => {
     loginForm.style.display = 'block';
     registrationForm.style.display = 'none';
+    loginFormVisible = true;
+    loginOption.style.display = 'none'
+    accountOption.style.display = 'none'
 });
 
 accountOption.addEventListener('click', () => {
     registrationForm.style.display = 'block';
     loginForm.style.display = 'none';
+    loginFormVisible = true;
+    accountOption.style.display = 'none'
+    loginOption.style.display = 'none'
 });
 
 window.addEventListener('click', (event) => {
     if (event.target === loginPopup) {
         loginPopup.style.display = 'none';
+        registrationForm.style.display = 'none';
+        loginForm.style.display = 'none';
+        loginFormVisible = false;
+    }
+});
+
+
+changeNickButton.addEventListener('click', () => {
+    overlay.style.display = 'block';
+});
+  
+closeChange.addEventListener('click', () => {
+    overlay.style.display = 'none';
+});
+  
+overlay.addEventListener('click', (event) => {
+    if (event.target === overlay) {
+      overlay.style.display = 'none';
     }
 });
