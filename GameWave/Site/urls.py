f28ab08d49d62dcca126034b429f489693d33897
reprise77 <semenzemskov03@yandex.ru>
2023-11-08 11:28:58 +1000

    Added games and personal profile on Django

--- GameWave/Site/urls.py
+++ GameWave/Site/urls.py
@@ -6,4 +6,12 @@ urlpatterns = [
     path("", views.index, name="home"),
     path("about/", views.about, name="about"),
     path("profile/", views.profile, name="profile"),
+    path("doom/", views.doom, name="doom"),
+    path("duke/", views.duke, name="duke"),
+    path("wolf/", views.wolf, name="wolf"),
+    path("mk/", views.mk, name="mk"),
+    path("doom2/", views.doom2, name="doom2"),
+    path("mario/", views.mario, name="mario"),
+    path("shadow-warrior/", views.shadow_warrior, name="shadow-warrior"),
+    path("heroes/", views.heroes, name="heroes"),
 ]
