87a4f81e7b016e78c36a0f0c89a811a148d61af8
reprise77 <semenzemskov03@yandex.ru>
2023-11-08 11:28:58 +1000

    Added games and personal profile on Django

--- GameWave/Site/views.py
+++ GameWave/Site/views.py
@@ -1,11 +1,54 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
+
 
 def index(request):
     return render(request, 'Site/catalog.html')
 
+
 def about(request):
     return render(request, 'Site/about.html')
 
+
 def profile(request):
     return render(request, 'Site/profile.html')
+
+
+def doom(request):
+    return render(request, 'Site/doom.html')
+
+
+def duke(request):
+    return render(request, 'Site/duke.html')
+
+
+def wolf(request):
+    return render(request, 'Site/wolf.html')
+
+
+def mk(request):
+    return render(request, 'Site/mk.html')
+
+
+def doom2(request):
+    return render(request, 'Site/doom2.html')
+
+
+def mario(request):
+    return render(request, 'Site/mario.html')
+
+
+def shadow_warrior(request):
+    return render(request, 'Site/shadow-warrior.html')
+
+
+def heroes(request):
+    return render(request, 'Site/heroes.html')
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'Site/catalog.html', context)
